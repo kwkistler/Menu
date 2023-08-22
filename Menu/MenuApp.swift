c73fd5a1f286d678515b8962a79d3b5373224254
Kraig Kistler <kraigwkistler@gmail.com>
2023-08-22 15:48:09 -0700

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Kraig Kistler on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
